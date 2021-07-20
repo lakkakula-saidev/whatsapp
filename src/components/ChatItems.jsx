16b44aef71aff04141514cef210a95b20cdc1dcd
Saidev Lakkakula <lakkakula.saidev@gmail.com>
2021-07-20 23:20:25 +0200

    removed unused imports

--- src/components/ChatItems.jsx
+++ src/components/ChatItems.jsx
@@ -6,17 +6,17 @@ import 'react-chat-elements/dist/main.css';
     export default function ChatItems() {
         return (
             <ChatList
-            className='chat-list'
-            dataSource={[
-                {
-                    avatar: 'https://facebook.github.io/react/img/logo.svg',
-                    alt: 'Reactjs',
-                    title: 'Facebook',
-                    subtitle: 'What are you doing?',
-                    date: new Date(),
-                    unread: 0,
-                },
-               
-            ]} />
+                className="chat-list"
+                dataSource={[
+                    {
+                        avatar: "https://source.unsplash.com/random",
+                        alt: "Reactjs",
+                        title: "Facebook",
+                        subtitle: "What are you doing?",
+                        date: new Date(),
+                        unread: 0
+                    }
+                ]}
+            />
         );
     }
